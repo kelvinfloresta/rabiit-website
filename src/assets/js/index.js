8b51db38aa46554b9573d769230176c85364fb8b
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-06-26 14:57:06 -0300

    feat: add responsive menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', () => {
   const menu = document.querySelector('.menu')
-  if (menu) {
-    menu.addEventListener('click', () => menu.classList.toggle('active'))
+  const links = document.querySelector('.links')
+  if (!menu || !links) {
+    return
   }
+
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    links.classList.toggle('active')
+    document.body.classList.toggle('no-overflow')
+  })
 })
