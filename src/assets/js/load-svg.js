79d95a45d6fed54ec6312fb873c5c116ab09a44e
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-06-30 03:13:23 -0300

    fix: babel async await compability

--- src/assets/js/load-svg.js
+++ src/assets/js/load-svg.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', () => {
-  async function loadSVG (svgName) {
+  function loadSVG (svgName) {
     const svgContainer = document.querySelector('.' + svgName)
     if (!svgContainer) {
       return
     }
-    const { data } = await window.axios.get(`/assets/img/${svgName}.svg`)
-    svgContainer.innerHTML = data
-    svgContainer.classList.add('active')
+
+    window.axios.get(`/assets/img/${svgName}.svg`).then(response => {
+      svgContainer.innerHTML = response.data
+      svgContainer.classList.add('active')
+    })
   }
 
   loadSVG('processor')
