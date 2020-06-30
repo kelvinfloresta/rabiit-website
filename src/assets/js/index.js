5ede362cf79c8e1b4bdee8027fe9dd9af70913fb
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-06-30 01:24:17 -0300

    fix: close menu on click link

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,9 +5,19 @@ window.addEventListener('load', () => {
     return
   }
 
-  menu.addEventListener('click', () => {
+  menu.addEventListener('click', toggleMenu)
+
+  const width = window.innerWidth > 0 ? window.innerWidth : screen.width
+  const isTablet = width <= 768
+  if (isTablet) {
+    links
+      .querySelectorAll('a')
+      .forEach(el => el.addEventListener('click', toggleMenu))
+  }
+
+  function toggleMenu () {
     menu.classList.toggle('active')
     links.classList.toggle('active')
     document.body.classList.toggle('no-overflow')
-  })
+  }
 })
