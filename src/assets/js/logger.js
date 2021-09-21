c92670061282c64b2cf64cc7c0af111e6465b29f
kelvinfloresta <kelvinf.contato@gmail.com>
2021-09-20 22:27:53 -0300

    fix(Logger.js): check undefined before call

--- src/assets/js/logger.js
+++ src/assets/js/logger.js
@@ -1,1 +1,2 @@
-window.Sentry.init({ dsn: 'https://7a27a4b92f94480fafa5751ce24677aa@o413563.ingest.sentry.io/5300344' })
+// eslint-disable-next-line no-unused-expressions
+window.Sentry?.init({ dsn: 'https://7a27a4b92f94480fafa5751ce24677aa@o413563.ingest.sentry.io/5300344' })
