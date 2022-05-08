6c15a44da43aee5c4bbc706f2bed772cdbe49596
kelvinfloresta <kelvinf.contato@gmail.com>
2022-05-07 23:36:46 -0300

    feat(contact-form): toggle card when clicking on checkbox

--- src/assets/js/pages/home.js
+++ src/assets/js/pages/home.js
@@ -6,12 +6,21 @@ window.addEventListener('load', () => {
   }
   const cards = document.querySelectorAll('.Home-products .cards-row .card')
   cards.forEach(card => card.addEventListener('click', selectProduct(card)))
+  const checkboxes = document.querySelectorAll('.contact-form input[type="checkbox"]')
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', selectCard(checkbox)))
 
   function selectProduct (card) {
     return () => {
       const { name } = card.dataset
       const checkbox = document.querySelector(`.form #${name}`)
       checkbox.checked = !checkbox.checked
+      card.classList.toggle('active')
+    }
+  }
+
+  function selectCard (checkbox) {
+    return () => {
+      const card = document.querySelector(`.Home-products .cards-row .card[data-name=${checkbox.id}]`)
       card.classList.toggle('active')
     }
   }
